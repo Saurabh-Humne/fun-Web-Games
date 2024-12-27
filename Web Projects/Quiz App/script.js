fc1b44f43214faf4c8b7b587c3173c504022e16e
Saurabh Humne <91553333+Saurabh-Humne@users.noreply.github.com>
2024-12-28 00:27:49 +0530

    Update script.js

--- Web Projects/Quiz App/script.js
+++ Web Projects/Quiz App/script.js
@@ -139,6 +139,8 @@ function selectAnswer(e){
 
 function showScore(){
     resetState();
+    const questionImage = document.getElementById("question-image");
+    questionImage.style.display = "none";
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
